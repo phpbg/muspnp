f421e83597d27801d0bc31d0ef3beab3d676190d
Samuel CHEMLA <chemla.samuel@gmail.com>
2022-11-06 16:11:18 +0100

    Quit app on window close on macOS

--- main.js
+++ main.js
@@ -115,24 +115,13 @@ const createWindow = () => {
 // Some APIs can only be used after this event occurs.
 app.on('ready', createWindow);
 
-// Quit when all windows are closed, except on macOS. There, it's common
-// for applications and their menu bar to stay active until the user quits
-// explicitly with Cmd + Q.
+// Quit when all windows are closed, including on macOS
 app.on('window-all-closed', () => {
     const upnp = getUpnpApi();
     upnp.ssdpStop(); //TODO only stop what we actually started...
     upnp.stop().catch(() => null).finally(() => {
         stopPowerSaver();
-        if (process.platform !== 'darwin') {
-            app.quit();
-        }
+        app.quit();
     })
 });
 
-app.on('activate', () => {
-    // On OS X it's common to re-create a window in the app when the
-    // dock icon is clicked and there are no other windows open.
-    if (BrowserWindow.getAllWindows().length === 0) {
-        createWindow();
-    }
-});
